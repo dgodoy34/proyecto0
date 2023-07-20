6258a73fb1570c2ab63d3bd9ec218b25a7ea27e4
Diego G <dgodoy34@gmail.com>
2023-07-20 02:47:56 -0300

    ultimos cambios

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
+//inicio de buscador de registro.html
+
+    // Array con los elementos a buscar
+    const elementos = [
+      { names: "Macetas", precioUnitario: 10 },
+      { names: "Fertilizantes", precioUnitario: 5 },
+      { names: "Plantas", precioUnitario: 20 },
+      { names: "Sustrato", precioUnitario: 8 },
+      { names: "Parafernalia", precioUnitario: 15 }
+    ];
+
+    document.getElementById("show").addEventListener("click", function() {
+      const productNames = document.getElementById("product").value.toLowerCase();
+      const product = elementos.find(stock => stock.names.toLowerCase() === productNames);
+
+      if (product) {
+        alert(`${product.names}, precio unitario $${product.precioUnitario}`);
+        console.log(`El producto ${product.names} está en la lista.`);
+      } else {
+        alert("Producto no encontrado.");
+        console.log("Producto no encontrado en la lista.");
+      }
+    });
+
+ //fin buscador
+
+ //inicio de registro.html
+
 function validateForm() {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let telefono = document.getElementById("telefono").value;
     let password = document.getElementById("password").value;
+    let validateForm = true;
 
     // Validar que el campo de nombre no esté vacío
     if (name.trim() === "") {
       alert("Por favor, ingresa tu nombre");
       //console.log("Por favor, ingresa tu nombre.")
       ev.preventDefault();
-      return false;
+      validateForm = false;
     }
 
     // Validar que el campo de correo electrónico sea válido
@@ -18,7 +47,7 @@ function validateForm() {
       alert("Por favor, ingresa un correo electrónico válido");
       //console.log("Por favor, ingresa un correo electrónico válido.")
       ev.preventDefault();
-      return false;
+      validateForm = false;
     }
 
     // Validar que el campo de telefono no esté vacío
@@ -26,7 +55,7 @@ function validateForm() {
       alert("Por favor, ingresa tu telefono");
       //console.log("Por favor, ingresa tu telefono.")
       ev.preventDefault();
-      return false;
+      validateForm = false;
     }
 
     // Validar que la contraseña tenga al menos 8 caracteres
@@ -34,18 +63,25 @@ function validateForm() {
       alert("La contraseña debe tener al menos 8 caracteres");
       //console.log("La contraseña debe tener al menos 8 caracteres.")
       ev.preventDefault();
-      return false;
+      validateForm = false;
     }
 
     // Si todo está correcto, el formulario se envía
+    if (validateForm()) {
+      alert("Formulario correcto gracias por registrarse...");
+      console.log("Formulario correcto gracias por registrarse...");
+    }
+
+    return validateForm;
+    /* Si todo está correcto, el formulario se envía
     alert("Formulario correcto gracias por registrarse...");
     console.log("Formulario correcto gracias por registrarse...")
-    return true;
+    return true;*/
   }
 
   // Función para generar contraseñas aleatorias
   function generarContrasenaAleatoria() {
-    const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     const longitudContrasena = 8;
     let contrasena = "";
 
@@ -57,25 +93,22 @@ function validateForm() {
     document.getElementById("password").value = contrasena;
     alert("Contraseña aleatoria generada: " + contrasena);
     console.log("Contraseña aleatoria generada: " + contrasena)
+  
   }
 
-  //comienzo de buscador
+  
 
-   function realizarBusqueda() {
-      let searchTerm = document.getElementById("search-input").value;
-      alert("Realizando búsqueda: " + searchTerm);
-      return false; // Evitar que el formulario se envíe y la página se recargue
-    }
-
-    //login
+    //login.html
 
     function iniciarSesion() {
       let username = document.getElementById("username").value;
       let password = document.getElementById("password").value;
 
-      // Aquí puedes implementar la lógica para verificar las credenciales del usuario
-      // Por ejemplo, puedes comparar el nombre de usuario y contraseña con valores predefinidos o hacer una solicitud a un servidor para validar las credenciales
+
+      // verificar las credenciales del usuario en servidor
+      console.log("\nUsuario: " + username + "\nContraseña: " + password)
+      //comparar el nombre de usuario y contraseña con valores predefinidos o hacer una solicitud a un servidor para validar las credenciales
 
       // En este ejemplo, simplemente mostramos una alerta con los datos del usuario ingresados
-      alert("Iniciando sesión...\nUsuario: " + username + "\nContraseña: " + password);
+      alert("Inicio sesión...\nUsuario: " + username + "\nContraseña: " + password);
     }
