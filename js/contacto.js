5dca35dc997944c3deef957845cddede631a7b7c
Diego G <dgodoy34@gmail.com>
2023-08-10 12:05:35 -0300

    contacto2 js

--- js/contacto.js
+++ js/contacto.js
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
   */
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const registrationForm = document.getElementById('registration-form');
     
     if (registrationForm) {
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const nombre = document.getElementById("nombre").value;
         const email = document.getElementById("email").value;
         const telefono = document.getElementById("tel").value;
-        const comentario = document.querySelector(".text-area").value;
+        const comentario = document.getElementById("comentario").value;
   
         // Validar campos
         if (nombre.trim() === "" || email.trim() === "" || telefono.trim() === "" || comentario.trim() === "") {
@@ -160,5 +160,143 @@ document.addEventListener('DOMContentLoaded', function() {
   function obtenerContactosLocalStorage() {
     const contactosJSON = localStorage.getItem("contactos");
     return contactosJSON ? JSON.parse(contactosJSON) : [];
+  }*/
+  /*document.addEventListener('DOMContentLoaded', function() {
+    const registrationForm = document.getElementById('registration-form');
+    
+    if (registrationForm) {
+      registrationForm.addEventListener('submit', async function(event) {
+        event.preventDefault();
+  
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const telefono = document.getElementById("tel").value;
+        const comentario = document.getElementById("comentario").value;
+  
+        // Validar campos
+        if (nombre.trim() === "" || email.trim() === "" || telefono.trim() === "" || comentario.trim() === "") {
+          Swal.fire('Error', 'Por favor, completa todos los campos correctamente', 'error');
+          return;
+        }
+  
+        // Comprobar si el email ya existe en el LocalStorage
+        if (existeEmailEnLocalStorage(email)) {
+          Swal.fire('Error', 'El email ya ha sido registrado.', 'error');
+          return;
+        }
+  
+        // Guardar en LocalStorage
+        guardarEnLocalStorage(nombre, email, telefono, comentario);
+  
+        // Inicializar EmailJS con tu ID de usuario
+        emailjs.init("fksMZpFl9TQtslnTL");
+  
+        const serviceID = 'default_service';
+        const templateID = 'template_p4clcsr';
+  
+        try {
+          const response = await emailjs.sendForm(serviceID, templateID, this);
+          if (response.status === 200) {
+            Swal.fire('Éxito', 'Formulario enviado correctamente', 'success');
+            registrationForm.reset();
+          } else {
+            Swal.fire('Error', 'Hubo un problema al enviar el formulario. Por favor, intenta nuevamente más tarde.', 'error');
+          }
+        } catch (error) {
+          console.error("Error al enviar el formulario:", error);
+          Swal.fire('Error', 'Hubo un problema al enviar el formulario. Por favor, intenta nuevamente más tarde.', 'error');
+        }
+      });
+    }
+  });
+  
+  // Función para verificar si el email ya existe en LocalStorage
+  function existeEmailEnLocalStorage(email) {
+    const contactosGuardados = obtenerContactosLocalStorage();
+    return contactosGuardados.some(contacto => contacto.email === email);
   }
+  
+  // Función para guardar el contacto en LocalStorage
+  function guardarEnLocalStorage(nombre, email, telefono, comentario) {
+    const nuevoContacto = { nombre, email, telefono, comentario };
+    const contactosGuardados = obtenerContactosLocalStorage();
+    contactosGuardados.push(nuevoContacto);
+    localStorage.setItem("contactos", JSON.stringify(contactosGuardados));
+  }
+  
+  // Función para obtener los contactos almacenados en LocalStorage
+  function obtenerContactosLocalStorage() {
+    const contactosJSON = localStorage.getItem("contactos");
+    return contactosJSON ? JSON.parse(contactosJSON) : [];
+  }*/
+  document.addEventListener('DOMContentLoaded', function() {
+    const registrationForm = document.getElementById('registration-form');
+    
+    if (registrationForm) {
+      registrationForm.addEventListener('submit', async function(event) {
+        event.preventDefault();
+  
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const telefono = document.getElementById("tel").value;
+        const comentario = document.getElementById("comentario").value;
+  
+        // Validar campos
+        if (nombre.trim() === "" || email.trim() === "" || telefono.trim() === "" || comentario.trim() === "") {
+          Swal.fire('Error', 'Por favor, completa todos los campos correctamente', 'error');
+          return;
+        }
+  
+        // Comprobar si el email ya existe en el LocalStorage
+        if (existeEmailEnLocalStorage(email)) {
+          Swal.fire('Error', 'El email ya ha sido registrado.', 'error');
+          return;
+        }
+  
+        // Guardar en LocalStorage
+        guardarEnLocalStorage(nombre, email, telefono, comentario);
+  
+        // Inicializar EmailJS con tu ID de usuario
+        emailjs.init("fksMZpFl9TQtslnTL");
+  
+        const serviceID = 'default_service';
+        const templateID = 'template_p4clcsr';
+  
+        try {
+          const response = await emailjs.sendForm(serviceID, templateID, this);
+          if (response.status === 200) {
+            Swal.fire('Éxito', 'Formulario enviado correctamente', 'success');
+            registrationForm.reset();
+          } else {
+            Swal.fire('Error', 'Hubo un problema al enviar el formulario. Por favor, intenta nuevamente más tarde.', 'error');
+          }
+        } catch (error) {
+          console.error("Error al enviar el formulario:", error);
+          Swal.fire('Error', 'Hubo un problema al enviar el formulario. Por favor, intenta nuevamente más tarde.', 'error');
+        }
+      });
+    }
+  });
+  
+  // Función para verificar si el email ya existe en LocalStorage
+  function existeEmailEnLocalStorage(email) {
+    const contactosGuardados = obtenerContactosLocalStorage();
+    return contactosGuardados.some(contacto => contacto.email === email);
+  }
+  
+  // Función para guardar el contacto en LocalStorage
+  function guardarEnLocalStorage(nombre, email, telefono, comentario) {
+    const nuevoContacto = { nombre, email, telefono, comentario };
+    const contactosGuardados = obtenerContactosLocalStorage();
+    contactosGuardados.push(nuevoContacto);
+    localStorage.setItem("contactos", JSON.stringify(contactosGuardados));
+  }
+  
+  // Función para obtener los contactos almacenados en LocalStorage
+  function obtenerContactosLocalStorage() {
+    const contactosJSON = localStorage.getItem("contactos");
+    return contactosJSON ? JSON.parse(contactosJSON) : [];
+  }
+  
+  
   
